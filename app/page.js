ae62e3764300298ebe2ba723edd2fc74705e2a73
Pankaj Sharma <iampankajsharma001@gmail.com>
2025-12-07 22:08:41 +0545

    changes page.js

--- app/page.js
+++ app/page.js
@@ -1,21 +1,11 @@
-"use client"
+"use client";
 import Form from "./Components/LOGIN/form";
-import { useRouter } from 'next/navigation';
-// import Signin from "./Components/LOGIN/Signin";
-
+import { Suspense } from "react";
 
 export default function Home() {
-
-  const router = useRouter();
-
- const handleClick =()=>{
-  router.push('/about')
- };
-   
   return (
-    <>
- <Form/>
- {/* <Signin/> */}
- {/* <button className=" m-auto flex py-1.5 px-4 rounded-lg bg-slate-900 text-white" onClick={handleClick}>ABout Section </button> */}
-  </>);
+    <Suspense>
+      <Form />
+    </Suspense>
+  );
 }
