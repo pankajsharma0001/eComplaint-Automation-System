25985607d9ce6c9b2ae3d9e7eb56dfafa8c2e24c
Pankaj Sharma <iampankajsharma001@gmail.com>
2025-12-07 22:03:26 +0545

    updated login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,21 +1,12 @@
-"use client"
-import Form from '../Components/LOGIN/form'
-import { useEffect, useState } from 'react'
+"use client";
+
+import { Suspense } from "react";
+import Login from "../Components/LOGIN/form"; // your code
 
 export default function LoginPage() {
-  const [showError, setShowError] = useState(true);
-
-  useEffect(() => {
-
-    const timer = setTimeout(() => {
-      setShowError(false);
-    }, 3000);
-    return () => clearTimeout(timer);
-  }, [])
   return (
-    <div>
-      {showError && <h1 className='text-center text-red-500'>PleaseLogin first</h1>}
-      <Form />
-    </div>
-  )
+    <Suspense>
+      <Login />
+    </Suspense>
+  );
 }
